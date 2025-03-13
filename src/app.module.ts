0be8e62b6ff3019acdcd0c4801b1fb3accb930a7
DSamuelQ <165442834+DSamuelQ@users.noreply.github.com>
2025-03-12 21:40:33 -0600

    db connection (#1)
    * db connection
    
    * Delete env/development.env
    
    * Delete package-lock.json
    
    * Delete package.json
    
    * Cambios a yarn y creacion de .env.sample

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { configurationMongo } from './configuration/configuration-mongo';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configurationMongo],
+      envFilePath: `.env`, //archivo para variables de entorno
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get('mongo.MONGO_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
