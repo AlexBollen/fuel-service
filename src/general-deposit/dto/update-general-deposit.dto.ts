c0121798319813d815b61d302a8e9c7efd7dfd7f
Alexander Bollen <alexander.bollen@dooply.io>
2025-05-17 20:26:23 -0600

    Add: Endpoint to supply fuel

--- src/general-deposit/dto/update-general-deposit.dto.ts
+++ src/general-deposit/dto/update-general-deposit.dto.ts
@@ -1,4 +1,29 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateGeneralDepositDto } from './create-general-deposit.dto';
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsUUID,
+} from 'class-validator';
 
-export class UpdateGeneralDepositDto extends PartialType(CreateGeneralDepositDto) {}
+export class UpdateGeneralDepositDto extends PartialType(
+  CreateGeneralDepositDto,
+) {
+  @ApiProperty({
+    description: 'Id del depósito general',
+    example: 'JD2IJ29SAD-SUPER',
+  })
+  @IsOptional()
+  @IsUUID()
+  idProducto?: string;
+
+  @ApiProperty({
+    description: 'Cantidad de compra para el depósito en galones',
+    example: 500.0,
+  })
+  @IsNotEmpty()
+  @IsNumber()
+  @IsOptional()
+  cantidad?: number;
+}
